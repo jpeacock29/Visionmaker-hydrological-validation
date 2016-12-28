6e288bc551874b0f94463c837e2e801dba642a79
Jacob Peacock <jpeacock29@gmail.com>
2016-12-27 22:29:54 -0500

    Add script version of ipynb; improve file names

--- make.sh
+++ make.sh
@@ -6,5 +6,8 @@ wget https://data.cityofnewyork.us/api/views/hemm-82xw/rows.csv?accessType=DOWNL
 mv rows.csv\?accessType\=DOWNLOAD inputs/311_Service_Requests_from_2015.csv
 # install any needed packages
 pip3 install -r requirements.txt
+# create a python script from the provided jupyter notebook
+jupyter nbconvert --to script analysis.ipynb
+echo "Warning: The analysis is memory intensive and may take a moment."
 # run the analysis
 python3 analysis.py

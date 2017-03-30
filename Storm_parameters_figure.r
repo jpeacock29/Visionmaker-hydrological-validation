3600e52d1142d026b5693c196fc207eb174c6478
Jacob Peacock <jpeacock29@gmail.com>
2017-03-29 21:40:24 -0400

    Clean up

---
+++ Storm_parameters_figure.r
@@ -0,0 +1,26 @@
+
+library("ggplot2")
+library("reshape2")
+# change size of inline plots
+options(repr.plot.width=8, repr.plot.height=3)
+
+df = read.csv('inputs/storm_modelling.csv')
+
+df$`Total Rainfall` = df$Intensity * df$Duration
+# filter out the 1600s data
+df = df[df$start > 1600,]
+# melt so we can use facet_grid
+df = melt(df, id.vars=c('model', 'rain_event_type', 'start', 'end', 'Duration'))
+
+g = ggplot(df) +
+    labs(x='Year', y=NULL) +
+    # tilde is just overloaded to do something completely different here
+    facet_grid(variable ~ rain_event_type, scales="free", switch="y") + 
+    geom_segment(aes(x=start, y=value, xend=end, yend=value, color=model)) +
+    #geom_text(aes(label=round(df$value, 2), x=start, y=value)) +
+    theme_minimal() +
+    theme(legend.position="bottom") + 
+    scale_colour_manual(values=c("black", "red")) +
+    scale_x_continuous(breaks=seq(1970, 2110, 40))
+
+ggsave("test.png", g, width=6.5, height=4, dpi=300, units="in")
--- make.sh
+++ make.sh
@@ -1,13 +1,15 @@
 # Input data will be placed in inputs. Figures and sheets will be placed in outputs_1.
-mkdir inputs outputs_1
+mkdir inputs outputs
+
 # download the data from Open NYC
 wget https://data.cityofnewyork.us/api/views/hemm-82xw/rows.csv?accessType=DOWNLOAD
+
 # rename and move the downloaded file
 mv rows.csv\?accessType\=DOWNLOAD inputs/311_Service_Requests_from_2015.csv
+
 # install any needed packages
 pip3 install -r requirements.txt
-# create a python script from the provided jupyter notebook
-jupyter nbconvert --to script analysis.ipynb
-echo "Warning: The analysis is memory intensive and may take a moment."
+
 # run the analysis
-python3 analysis.py
+echo "Warning: The analysis is memory intensive and may take a moment."
+python3 Testing_flood_predictions.py
